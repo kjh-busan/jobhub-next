d3895f4984913c20a5ca28254fcecb6925e9d43b
bit-ksw <bit7521@gmail.com>
2019-07-03 00:56:30 +0900

    domain change test amazon ip

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
-const backUrl = process.env.NODE_ENV === 'production' ? 'https://api.nodebird.com' : 'http://localhost:3065';
+const backUrl = process.env.NODE_ENV === 'production' ? '3.113.140.58' : 'http://localhost:3065';
 
 export { backUrl };
